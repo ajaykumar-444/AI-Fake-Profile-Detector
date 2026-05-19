007b83562bf8461256d6e02cd53170c1ba32653d
ajaykumar-444 <ajaynaidumathi99@gmail.com>
2026-05-19 09:18:15 +0530

    Fixed gunicorn startup

--- backend/app.py
+++ backend/app.py
@@ -268,14 +268,3 @@ def scan_profile():
 # -----------------------------
 # MAIN
 # -----------------------------
-
-if __name__ == '__main__':
-
-    port = int(
-        os.environ.get("PORT", 5000)
-    )
-
-    app.run(
-        host='0.0.0.0',
-        port=port
-    )
